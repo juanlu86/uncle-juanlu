5180462e463865ab3f0741635811a1a10e18acb3
Juan Luis Lopez Muñoz <juan.luis.lopez.munoz@philips.com>
2018-02-08 20:22:08 +0100

    Writing and reading user defaults data

--- Data Persistence/Data Persistence/ViewController.swift
+++ Data Persistence/Data Persistence/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
+struct UserDefaultsKeys {
+    static let boolKey = "Key1"
+    static let intKey = "Key2"
+    static let objectKey = "Key3"
+    static let arrayKey = "Key4"
+}
+
 class ViewController: UIViewController {
 
+    let userDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addDataToUserDefaults() {
+        userDefaults.set(true, forKey: UserDefaultsKeys.boolKey)
+        userDefaults.set(123, forKey: UserDefaultsKeys.intKey)
+        userDefaults.set("Some Object", forKey: UserDefaultsKeys.objectKey)
+        userDefaults.set([1, 2, 3, 4], forKey: UserDefaultsKeys.arrayKey)
+        
+        userDefaults.synchronize()
     }
-
-
+    
+    @IBAction func readDataFromUserDefaults() {
+        let myBool = userDefaults.bool(forKey: UserDefaultsKeys.boolKey)
+        let myInt = userDefaults.integer(forKey: UserDefaultsKeys.intKey)
+        let myObject = userDefaults.object(forKey: UserDefaultsKeys.objectKey)
+        let myArray = userDefaults.object(forKey: UserDefaultsKeys.arrayKey)
+        
+        print("\(myBool), \(myInt), \(String(describing: myObject)), \(String(describing: myArray))")
+    }
 }
 
